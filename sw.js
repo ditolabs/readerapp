aae262224b0f3bc10dc732f23c2a3b0174139c8d
ditolabs <halodito@gmail.com>
2026-06-17 17:44:01 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,12 @@
+const CACHE='readerapp-v2';
+const BASE=self.location.pathname.replace('/sw.js','');
+const ASSETS=[BASE+'/readerapp.html',BASE+'/manifest.json'];
+self.addEventListener('install',e=>{
+  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));
+});
+self.addEventListener('activate',e=>{
+  e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
+});
+self.addEventListener('fetch',e=>{
+  e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).catch(()=>caches.match(BASE+'/readerapp.html'))));
+});
